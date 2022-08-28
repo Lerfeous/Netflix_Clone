5503ef191699c3ffec419e09b639710e118b2cba
YASH BHARDWAJ <lerfeous@gmail.com>
2022-08-28 09:54:38 +0530

    commit

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,17 +1,28 @@
-import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined, Translate } from '@material-ui/icons'
 import ListItem from "../list_item/Listitem.jsx"
-import React from 'react'
+import React, { useRef } from 'react'
 import "./list.scss";
 
 export default function List() {
+
+  const listRef = useRef();
+
+  const handleClick =(direction)=>{
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === "left")
+    {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    console.log(distance)
+  }
   return (
     <div className='list'>
         <span className="listTitle">
             Continue To Watch
         </span>
         <div className="wrapper">
-            <ArrowBackIosOutlined className='sliderArrow left'/>
-            <div className="container">
+            <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")}/>
+            <div className="container" ref={listRef}>
               <ListItem/> 
               <ListItem/>
               <ListItem/>
@@ -29,7 +40,7 @@ export default function List() {
               <ListItem/>
               <ListItem/>
             </div>
-            <ArrowForwardIosOutlined className='sliderArrow right'/>
+            <ArrowForwardIosOutlined className='sliderArrow right' onClick={()=>handleClick("right")}/>
         </div>
     </div>
   )
